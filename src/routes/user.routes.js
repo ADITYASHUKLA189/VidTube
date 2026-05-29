bbc7edbe2d2eec74eae401bb8dd5fa15dd6b7502
Aditya Shukla <Shukladitya22@gmail.com>
2026-05-29 16:40:20 +0530

    added functionality of user registration

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,25 @@
 import {Router} from 'express';
 import {registerUser} from '../controllers/user.controller.js';
-
+import upload from '../middlewares/multer.middleware.js';
 const router=Router();
 
-router.route("/register").post(registerUser);
+
+//register se phele multer middleware chalega jo
+//  ki image ko handle karega and then registerUser
+//  controller chalega jo ki user registration ka logic handle karega
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+);
 
 
 
